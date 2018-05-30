e8a4caedc93b72cbe9c1a327258162e2664a460c
ArsyaNoorianda <arsyha.noorianda@gmail.com>
2018-05-31 00:43:21 +0700

    itung jumlah kata

---
+++ W3D3Excercise7.js
@@ -0,0 +1,14 @@
+function hitungJumlahKata (kalimat) {
+  var hasil = kalimat.split(' ');
+  return hasil.length;
+}
+
+console.log(hitungJumlahKata('I'))
+
+// TEST CASES
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
+
